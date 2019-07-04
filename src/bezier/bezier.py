9599cd5a3bcf6579c8d6334c40d8f5b8e58257f4
pearisgreen <gerrit.proessl@gmail.com>
2019-07-04 15:09:45 +0200

    # Update bezier

--- src/bezier/bezier.py
+++ src/bezier/bezier.py
@@ -3,27 +3,33 @@ import cairo
 
 points = [np.array([1,1]), np.array([1,0]), np.array([0,0]), np.array([0,1])]
 
-dt = 0.1
+dt = 0.01
 
 print(points)
 
-def copy_points(points):
-    res = []
-    for p in points:
-        res.append(np.copy(p))
-    return res
+def copy_points(dst, src):
+    print("dl: " + str(len(dst)) + " sl: " + str(len(src)))
+    if(len(dst) != len(src)):
+        raise Exception("trying to copy arrays of different length")
+    for i in range(len(src)):
+        dst[i] = np.copy(src[i])
 
-def bezier(p, t):
-    points = copy_points(p)
+def bezier_point(p, t, buf):
+    copy_points(buf, p)
     i = len(points) - 1
     for x in range(i):
         for y in range(i - x):
-            a = points[y] * (1 - t)
-            b = points[y + 1] * t
-            points[y] = a + b
-    return points[0]
+            a = buf[y] * (1 - t)
+            b = buf[y + 1] * t
+            buf[y] = a + b
+    return buf[0]
 
-
+def bezier(p, dt):
+    buf = [None] * len(p)
+    print("here")
+    for i in np.arange(dt, 1 + dt, dt):
+        p = bezier_point(p, i, buf)
+        yield p
 
 WIDTH, HEIGHT = 256, 256
 
@@ -34,12 +40,11 @@ ctx.scale(WIDTH, HEIGHT)  # Normalizing the canvas
 
 ctx.move_to(points[0][0], points[0][1])
 
-for i in np.arange(0, 1, dt):
-    p = bezier(points, i)
-    print(i)
+for p in bezier(points, dt):
     print("points: " + str(points))
-    print("p: " + str(p))
-    ctx.move_to(p[0], p[1])
+    print("p: " + str(p[0]) + " " + str(p[1]))
+    ctx.line_to(p[0], p[1])
+
 
 ctx.set_source_rgb(0.3, 0.2, 0.5)  # Solid color
 ctx.set_line_width(0.02)
