2c26c86f58e0a82af8a936d0e650459a56b339fd
pearisgreen <gerrit.proessl@gmail.com>
2019-07-05 13:27:50 +0200

    + Add svg parser points

--- src/svg/parser.py
+++ src/svg/parser.py
@@ -1,18 +1,28 @@
 from svgpathtools import svg2paths
+import numpy as np
+import cairo
+
 paths, attributes = svg2paths("example/note.svg")
 
-SAMPLES_PER_PX = 1
+WIDTH, HEIGHT = 256, 256
 
-myPaths = {}
-for path,attr in zip(paths, attributes):
-    myPathList = []
-    pathLength = path.length()
-    pathColour = attr['stroke']
-    numSamples = int(pathLength * SAMPLES_PER_PX)
-    for i in range(numSamples):
-        #parametric length = ilength(geometric length)
-        myPathList.append(path.point(path.ilength(pathLength * i / (numSamples-1))))
-    myPaths[pathColour] = np.array(myPathList)
+surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
+ctx = cairo.Context(surface)
+
+ctx.scale(WIDTH, HEIGHT)  # Normalizing the canvas
+
+ctx.move_to(points[0][0], points[0][1])
+
+for p in paths:
+    for x in np.arange(0, 1, 0.01):
+        print(type(p.point(x)))
+        print(p.point(x).real)
+        print(p.point(x).imag)
 
 
-print(myPaths)
+
+ctx.set_source_rgb(0.3, 0.2, 0.5)  # Solid color
+ctx.set_line_width(0.02)
+ctx.stroke()
+
+surface.write_to_png("example.png")  # Output to PNG
