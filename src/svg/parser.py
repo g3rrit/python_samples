9b0480adaecf265d74b3e7a467a0b631f1fcb2ce
pearisgreen <gerrit.proessl@gmail.com>
2019-07-07 22:34:56 +0200

    # Update small svg update

--- src/svg/parser.py
+++ src/svg/parser.py
@@ -1,5 +1,6 @@
 from svgpathtools import svg2paths
 import numpy as np
+import math
 import cairo
 
 paths, attributes = svg2paths("example/note.svg")
@@ -11,18 +12,15 @@ ctx = cairo.Context(surface)
 
 ctx.scale(WIDTH, HEIGHT)  # Normalizing the canvas
 
-ctx.move_to(points[0][0], points[0][1])
+ctx.set_source_rgb(0.3, 0.2, 0.5)  # Solid color
 
 for p in paths:
     for x in np.arange(0, 1, 0.01):
         print(type(p.point(x)))
         print(p.point(x).real)
         print(p.point(x).imag)
+        ctx.arc(p.point(x).real, p.point(x).imag, 0.01, 2 * math.pi)
+        ctx.fill()
 
-
-
-ctx.set_source_rgb(0.3, 0.2, 0.5)  # Solid color
-ctx.set_line_width(0.02)
-ctx.stroke()
 
 surface.write_to_png("example.png")  # Output to PNG
