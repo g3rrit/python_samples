7de8b571fb61579ea4c445474588252840436dde
pearisgreen <gerrit.proessl@gmail.com>
2019-07-08 17:00:41 +0200

    # Update svg parser

--- src/svg/parser.py
+++ src/svg/parser.py
@@ -14,13 +14,30 @@ ctx.scale(WIDTH, HEIGHT)  # Normalizing the canvas
 
 ctx.set_source_rgb(0.3, 0.2, 0.5)  # Solid color
 
+points = []
 for p in paths:
-    for x in np.arange(0, 1, 0.01):
-        print(type(p.point(x)))
-        print(p.point(x).real)
-        print(p.point(x).imag)
-        ctx.arc(p.point(x).real, p.point(x).imag, 0.01, 2 * math.pi)
-        ctx.fill()
+    for x in np.arange(0, 1, 0.001):
+        points.append(np.array([p.point(x).real, p.point(x).imag]))
+
+def normalize(pa):
+    xmax = 0
+    ymax = 0
+    for p in pa:
+        if p[0] > xmax:
+            xmax = p[0]
+        if p[1] > ymax:
+            ymax = p[1]
+
+    for p in pa:
+        p[0] = p[0] / xmax
+        p[1] = p[1] / ymax
+
+
+normalize(points)
+
+for p in points:
+    ctx.arc(p[0], p[1], 0.003, 0, 2 * math.pi)
+    ctx.fill()
 
 
 surface.write_to_png("example.png")  # Output to PNG
